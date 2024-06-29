201e6bb42e4eb96233fede0b25b4e2f1dcfa9e21
Neha-0713 <nrai52703@gmail.com>
2024-06-29 17:19:02 +0530

    basic js

---
+++ conditions.js
@@ -0,0 +1,14 @@
+n=344;
+if(n%2===0){
+    console.log('even');
+    something='hello';
+    let x='nice'
+    const pi=3.14;
+    //pi=345.5;
+}
+else{
+    console.log('odd');
+}
+console.log(something);
+//console.log(x);
+
---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Hello World');
+console.log('javascript');
+a=637;
+console.log(a);
+console.log(typeof a);
+myname='neha'
+console.log(myname);
+console.log(typeof myname);
+islogin=true;
+console.log(islogin);
+console.log(2**4);
+console.log(1=="1");
+console.log(1==='1');
+console.log(34+'45');
